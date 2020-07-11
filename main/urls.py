4daa9e5e56d1ed5960ab60d75a8afe29bff92324
Asylbek Abdyrakmanov <asylbekmg@gmail.com>
2020-07-12 00:31:26 +0600

    new changes

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from django.contrib import admin
 from classroom.views import classroom, students, teachers
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
