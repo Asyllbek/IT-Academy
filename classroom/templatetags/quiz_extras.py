f8986c5cb1c8d991c41665259dc87e408d904108
Asyllbek <asylbekmg@gmail.com>
2022-12-08 18:18:54 +0600

    Optimize functions

--- classroom/templatetags/quiz_extras.py
+++ classroom/templatetags/quiz_extras.py
@@ -1,36 +1,48 @@
-from django import template
-from django.db.models import Sum
-
-from classroom.models import StudentAnswer
 import hashlib
+from collections import (
+    namedtuple,
+)
+
+from django import template
+from django.db.models import (
+    Sum,
+)
+
+from classroom.models import (
+    StudentAnswer,
+)
 
 register = template.Library()
 
 
 @register.simple_tag
 def marked_answer(user, opt):
-    studentanswer = StudentAnswer.objects.filter(student=user.student, answer=opt)
-    if studentanswer:
-        if opt.is_correct:
-            return 'correct'
-        return 'wrong'
-    return ''
+    student_answer_exists = StudentAnswer.objects.filter(student=user.student, answer=opt).exists()
+
+    return (
+        'correct' if opt.is_correct else 'wrong'
+    ) if student_answer_exists else None
 
 
 @register.filter
 def gravatar_url(username, size=40):
-    # TEMPLATE USE:  {{ email|gravatar_url:150 }}
-    username_hash = hashlib.md5(username.lower().encode('utf-8')).hexdigest()
+    username_bytes = username.lower().encode('utf-8')
+
+    username_hash = hashlib.md5(username_bytes).hexdigest()
+
     return f"https://www.gravatar.com/avatar/{username_hash}?s={size}&d=identicon"
 
 
 @register.filter
 def top_subject(taken_quizzes):
+    Subject = namedtuple('Subject', ['name', 'score'])
+
     subjects = taken_quizzes.values('quiz__subject__name') \
         .annotate(score=Sum('score')) \
         .order_by('-score')
+
     if subjects:
-        name = subjects[0]['quiz__subject__name']
-        score = subjects[0]['score']
-        return f"{name} x {score}"
-    return ""
+        subject = Subject(subjects[0]['quiz__subject__name'], subjects[0]['score'])
+        return f"{subject.name} x {subject.score}"
+
+    return None
